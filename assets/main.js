426721138f2e23e79bf00aae5289605f7f1340bc
Thestudc <43852447+Thestudc@users.noreply.github.com>
2022-04-23 01:54:18 +0200

    fifth-commit

--- assets/main.js
+++ assets/main.js
@@ -98,16 +98,29 @@ function openNav(movie){
             if(videoData.results.length > 0)
             {
                 var embed = [];
-                videoData.results.forEach(video => {
+                var dots = [];
+                videoData.results.forEach((video, idx) => {
                     let {key, name, site} = video;
                     if(site == 'YouTube')
                     {
                         embed.push(`
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}" title="${name}" class="embed hide" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        `)
+                        dots.push(`
+                        <span class="dot">${idx + 1}</span>
                         `)
                     }
                 })
-                overlayContent.innerHTML  = embed.join('');
+                var content = `
+                <h1 class ="no-results">${movie.original_title}</h1>
+                <br>
+                ${embed.join('')}
+                <br>
+                <div class = "dots">${dots.join('')}</div>
+                `;
+                overlayContent.innerHTML  = content;
+                activeSlide = 0;
+                showVideos();
             }
             else
             {
@@ -120,6 +133,64 @@ function openNav(movie){
 function closeNav(){
     document.getElementById("myNav").style.width = "0%";
 }
+
+var activeSlide = 0;
+var totalVideos = 0;
+function showVideos(){
+    let embedClasses = document.querySelectorAll('.embed');
+    let dots = document.querySelectorAll('.dot');
+    totalVideos = embedClasses.length;
+    embedClasses.forEach((embedTag, idx) => {
+        if(activeSlide == idx)
+        {
+            embedTag.classList.add('show');
+            embedTag.classList.remove('hide');
+        }
+        else
+        {
+            embedTag.classList.add('hide');
+            embedTag.classList.remove('show');
+        }
+    })
+
+    dots.forEach((dot, indx) => {
+        if(activeSlide == indx)
+        {
+            dot.classList.add('active');
+        }
+        else
+        {
+            dot.classList.remove('active');
+        }
+    })
+}
+
+const leftArr = document.getElementById('left-arrow');
+const rightArr = document.getElementById('right-arrow');
+
+leftArr.addEventListener('click', () => {
+    if(activeSlide > 0)
+    {
+        activeSlide --;
+    }
+    else
+    {
+        activeSlide = totalVideos - 1;
+    }
+    showVideos();
+})
+
+rightArr.addEventListener('click', () => {
+    if(activeSlide < (totalVideos - 1))
+    {
+        activeSlide ++;
+    }
+    else
+    {
+        activeSlide = 0;
+    }
+    showVideos();
+})
 
 function getColor(vote_average)
 {
